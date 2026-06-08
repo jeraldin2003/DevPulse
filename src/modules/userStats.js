77fda8a29a91184bc0b44b9cd9e6e41f385fac2e
jeraldin2003 <jeraldin2003@gmail.com>
2026-06-08 15:02:10 +0530

    feat(modules): Optimizes all modules with ES6 features

--- src/modules/userStats.js
+++ src/modules/userStats.js
@@ -1,42 +1,9 @@
+const getTotalUsers = (users) => users.length;
 
-  function getTotalUsers(users) {
-    let count = 0;
-  
-    for (let i = 0; i < users.length; i++) {
-      count = count + 1;
-    }
-  
-    return count;
-  }
-  
-  function getTotalCompanies(users) {
-    const companyNames = [];
-  
-    for (let i = 0; i < users.length; i++) {
-      const name = users[i].company.name;
-      let isDuplicate = false;
-  
-      for (let j = 0; j < companyNames.length; j++) {
-        if (companyNames[j] === name) {
-          isDuplicate = true;
-          break;
-        }
-      }
-  
-      if (!isDuplicate) {
-        companyNames.push(name);
-      }
-    }
-  
-    return companyNames.length;
-  }
-  
-  export function userStats(users) {
-    const totalUsers = getTotalUsers(users);
-    const totalCompanies = getTotalCompanies(users);
-  
-    return {
-      totalUsers,
-      totalCompanies,
-    };
-  }
+const getTotalCompanies = (users) =>
+  new Set(users.map(({ company: { name } }) => name)).size;
+
+export const userStats = (users) => ({
+  totalUsers: getTotalUsers(users),
+  totalCompanies: getTotalCompanies(users),
+});
